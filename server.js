f6f0d942805c450736c54602b82579e1a5503b0a
Parikshit09-coder <gurudesai2005@gmail.com>
2025-04-23 07:03:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/api/auth',authRoutes);
 app.use('/api/patients',authentication,patient_info);
 app.use('/api/store_op',authentication,firebase_api);
 
-mongoose.connect(MONGO_URL, { useUnifiedTopology: true })
+mongoose.connect(MONGO_URL, { useNewUrlParser: true })
     .then(() => { console.log("Connected to database"); })
     .catch((err) => {
         console.error("Error in connecting to database:", err);
