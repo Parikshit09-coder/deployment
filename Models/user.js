05e730805227516d8995d80d97348820638f9db6
Parikshit09-coder <gurudesai2005@gmail.com>
2025-04-24 17:22:57 +0530

    Update user.js

--- Models/user.js
+++ Models/user.js
@@ -6,14 +6,27 @@ const UserSchema = new mongoose.Schema({
     password:{type:String,required:true},
 });
 
-UserSchema.pre('save', async function (next) { 
-    const user = this;
-    if (user.isModified('password') || user.isNew) {
-       
-        const hash = await bcrypt.hash(user.password, 10);
-        user.password = hash;
+UserSchema.pre('save', async function (next) {
+  const user = this;
+
+
+  if (!user.isModified('password') && !user.isNew) return next();
+
+  try {
+    // Ensure password is a valid string
+    if (typeof user.password !== 'string') {
+      throw new Error('Password must be a string');
     }
-    next();})
+
+
+    const hash = await bcrypt.hash(user.password, 10);
+    user.password = hash;
+
+    next(); // Proceed to save
+  } catch (err) {
+    next(err); 
+  }
+});
 
 UserSchema.methods.isValidPassword = async function (password){
         return await bcrypt.compare(password,this.password);
